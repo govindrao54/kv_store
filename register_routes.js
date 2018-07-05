3d1e3ffd47c9a8e804e80b43e7451d0099f97497
govindrao54 <govindrao54@gmail.com>
2018-07-05 18:29:55 +0530

    adding basic security and cookie parser

--- register_routes.js
+++ register_routes.js
@@ -2,12 +2,14 @@
  * @Author: govindrao54
  * @Date:   2018-06-30 02:03:02
  * @Last Modified by:   govindrao54
- * @Last Modified time: 2018-07-01 03:53:02
+ * @Last Modified time: 2018-07-03 10:28:56
  */
 
 const rv = require('request-validation');
 const mw = require('./middlewares/common_mw.js');
-var bodyParser = require('body-parser')
+const helmet = require('helmet');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 // do this just once
 const baseJoi = require('joi');
@@ -23,12 +25,16 @@ module.exports = function(app) {
         params: { allowUnknown: false }
     });
 
-    // parsing the req.body
+    // basic security
+    app.use(helmet());
+
+    // parsing the req.body and cookies
     app.use(bodyParser.json({ limit: '5mb' }));
     app.use(bodyParser.urlencoded({
         extended: true,
         limit: '5mb'
     }));
+    app.use(cookieParser());
 
     // object routes
     app.use('/object', require('./routes/object.js'));
